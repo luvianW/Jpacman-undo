9076a19d9083a4f5f8417743ca557ad1ce465d03
luvianwang <luvianwang@dhcp-128-189-194-251.ubcsecure.wireless.ubc.ca>
2016-10-18 18:00:53 -0700

    add UndoStoryTest

--- src/test/java/org/jpacman/test/framework/accept/UndoStoryTest.java
+++ src/test/java/org/jpacman/test/framework/accept/UndoStoryTest.java
@@ -1,13 +1,52 @@
 package org.jpacman.test.framework.accept;
 
 import org.jpacman.framework.ui.MainUI;
+import org.jpacman.undo.UndoablePacman;
+import org.junit.Test;
 
 public class UndoStoryTest extends MovePlayerStoryTest {
 
+	UndoablePacman newUndoPac;
+
 	@Override
 	public MainUI makeUI() {
+		newUndoPac = new UndoablePacman();
+		return newUndoPac;
+	}
 
-		return null;
+	@Override
+	public MainUI getUI() {
+		return newUndoPac;
+
+	}
+
+	@Test
+	public void undo_scenarios_71_tests() {
+
+	}
+
+	@Test
+	public void undo_scenarios_72_tests() {
+
+	}
+
+	@Test
+	public void undo_scenarios_73_tests() {
+
+	}
+
+	@Test
+	public void undo_scenarios_74_tests() {
+
+	}
+
+	@Test
+	public void undo_scenarios_75_tests() {
+
+	}
+
+	@Test
+	public void undo_scenarios_76_tests() {
 
 	}
 
