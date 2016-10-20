5ee1e4abbfba25c788420f1c5bdf72d07be5fcc4
chuxi <chuxitina@hotmail.com>
2016-10-19 22:22:37 -0700

    undoableGame & undoStack implemented

--- src/main/java/org/jpacman/undo/UndoableGame.java
+++ src/main/java/org/jpacman/undo/UndoableGame.java
@@ -1,13 +1,46 @@
 package org.jpacman.undo;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
 import org.jpacman.framework.model.*;
 
 public class UndoableGame extends Game {
+	
+	Deque<undoStack> States = new ArrayDeque<undoStack>();
 	public void undo(){
-		
+	    undoStack preState = States.pop();
+	    int prePoint = preState.getPoints();
+	    Tile prePlayerTile = preState.getPlayerLocation();
+	    List<Tile> ghostTile = new ArrayList<Tile>();
+	    for (Tile ghost : preState.getGhostLocation()){
+		      ghostTile.add(ghost);
+		}
+	    
+	    Player player = this.getPlayer();
+	    if(player.getPoints() > prePoint){
+	    	Food food = new Food();
+	    	food.occupy(player.getTile());
+	    	this.getPointManager().consumePointsOnBoard(player, player.getPoints()-prePoint);
+	    }
+	    
+	    player.deoccupy();
+	    player.occupy(prePlayerTile);
+	    
+	    for (Ghost ghost : this.getGhosts()){
+		     ghost.deoccupy();
+		     ghost.occupy(ghostTile.remove(0));
+		      
+		}
 	}
 	
 	@Override
 	public void moveGhost(Ghost theGhost, Direction dir){
+	//push stack
+		undoStack currentState = new undoStack();
+		currentState.pushStack(this);
+		States.push(currentState);
 		super.moveGhost(theGhost, dir);
 	}
 	
