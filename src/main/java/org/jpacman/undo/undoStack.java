2d6152fc36af619f5030829c1866316903be4953
chuxi <chuxitina@hotmail.com>
2016-10-20 12:35:41 -0700

    add a feature

--- src/main/java/org/jpacman/undo/undoStack.java
+++ src/main/java/org/jpacman/undo/undoStack.java
@@ -3,41 +3,40 @@ package org.jpacman.undo;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jpacman.framework.model.*;
+import org.jpacman.framework.model.Ghost;
+import org.jpacman.framework.model.Tile;
 
 class undoStack {
-    //UndoableGame Game;
-	 Tile playerTile;
-	 int points;
+	// UndoableGame Game;
+	Tile playerTile;
+	int points;
 	private List<Tile> ghostTile = new ArrayList<Tile>();
-	
-	public undoStack(){
+
+	public undoStack() {
 		playerTile = null;
 		points = 0;
-		ghostTile = null;
 	}
-	
-	public void pushStack(UndoableGame Game){
+
+	public void pushStack(UndoableGame Game) {
 		points = Game.getPlayer().getPoints();
 		playerTile = Game.getPlayer().getTile();
-		for (Ghost ghost : Game.getGhosts()){
-		      ghostTile.add(ghost.getTile());
+		for (Ghost ghost : Game.getGhosts()) {
+			ghostTile.add(ghost.getTile());
 		}
-		
+
 	}
-	
-	
-	public Tile getPlayerLocation(){
+
+	public Tile getPlayerLocation() {
 		return playerTile;
 	}
-	
-	public List<Tile> getGhostLocation(){
+
+	public List<Tile> getGhostLocation() {
 		List<Tile> result = new ArrayList<Tile>();
 		result.addAll(ghostTile);
 		return result;
 	}
-	
-	public int getPoints(){
+
+	public int getPoints() {
 		return points;
 	}
 }
